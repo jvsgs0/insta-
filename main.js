c0ef4fe07e9f6b17987a42cbacbcc75fc6c3be20
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2024-08-28 18:35:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function app(){
+    window.location.href = "https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3D2BCFFB18-A682-4D34-91F8-5CDC8BC21DE2%26utm_campaign%3DunifiedHome%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge%26original_referrer%3Dhttps%253A%252F%252Fwww.google.com%252F&pli=1";
+}
+
+function microsoft(){
+    window.location.href = "ms-windows-store://pdp/?productid=9nblggh5l9xt&referrer=appbadge&source=www.instagram.com&mode=mini&pos=0%2C0%2C1366%2C728";
+}
